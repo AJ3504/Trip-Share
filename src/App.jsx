f38d1ac0c59ca371af84eaf563411fa8cf4b7c41
kkotburi <128236313+kkotburi@users.noreply.github.com>
2023-07-17 17:50:14 +0900

    New draft

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import "./App.css";
-import React from "react";
-import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import React from 'react';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 
 const containerStyle = {
-  width: "400px",
-  height: "400px",
+  width: '400px',
+  height: '400px'
 };
 
 const center = {
   lat: -3.745,
-  lng: -38.523,
+  lng: -38.523
 };
 
 function App() {
   const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    googleMapsApiKey: "AIzaSyBVHVR_jL_0yrOULYr2cOQk3ucM5LyhqXQ",
+    id: 'google-map-script',
+    googleMapsApiKey: 'AIzaSyBVHVR_jL_0yrOULYr2cOQk3ucM5LyhqXQ'
   });
 
   const [map, setMap] = React.useState(null);
@@ -26,11 +25,11 @@ function App() {
       navigator.geolocation.getCurrentPosition((position) => {
         setUserLocation({
           lat: position.coords.latitude,
-          lng: position.coords.longitude,
+          lng: position.coords.longitude
         });
       }, console.error);
     } else {
-      console.error("Geolocation is not supported by this browser.");
+      console.error('Geolocation is not supported by this browser.');
     }
   }, []);
 
@@ -46,13 +45,7 @@ function App() {
   }, []);
 
   return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={10}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
-    >
+    <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10} onLoad={onLoad} onUnmount={onUnmount}>
       {/* 사용자의 위치가 있다면 Marker를 사용하여 지도에 표시합니다. */}
       {userLocation && <Marker position={userLocation} />}
     </GoogleMap>
