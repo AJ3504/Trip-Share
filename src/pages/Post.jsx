9fcdca0a34472ae6a47e23bd9e12dee89117b7b3
AJ3504 <plzgimmeajob@gmail.com>
2023-07-18 20:36:22 +0900

    Chore/Initiate postDetailPage based on KaKaoMap code

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Post = () => {
-  return <div>Post</div>;
+  return (
+    <>
+      <h1>post 페이지 시작합니다!</h1>
+      <h1>시작합니다!</h1>
+    </>
+  );
 };
 
 export default Post;
