b8fb9ae684cf8838f81257c6bc80d62418d04dfa
parkjei <parkjei94@gmail.com>
2023-07-18 15:14:14 +0900

    Docs/add Header&Layout file

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -3,15 +3,22 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from '../pages/Main';
 import Post from '../pages/Post';
 import Detail from '../pages/Detail';
+// import MyPage from '../pages/MyPage';
+import GlobalStyle from '../GlobalStyle';
+import Layout from './Layout';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/post" element={<Post />} />
-        <Route path="/:id" element={<Detail />} />
-      </Routes>
+      <GlobalStyle />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/post" element={<Post />} />
+          <Route path="/:id" element={<Detail />} />
+          {/* <Route path="/:id" element={<MyPage />} /> */}
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 };
