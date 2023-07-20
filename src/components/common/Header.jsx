f6a226dcf1ec0f95022067881a3ff523ac23bf82
parkjei <parkjei94@gmail.com>
2023-07-20 16:21:20 +0900

    Design/Change header structure

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -5,6 +5,8 @@ import { useNavigate } from 'react-router-dom';
 import { styled } from 'styled-components';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { auth } from '../../service/firebase';
+import { useSelector } from 'react-redux';
+import UserInfoModal from '../authentication/UserInfoModal';
 
 const Header = () => {
   const navigate = useNavigate();
@@ -24,15 +26,13 @@ const Header = () => {
     setCurrentUser(false);
   };
 
-  const handleProfileClick = () => {
-    navigate('/mypage');
-  };
-
   const handleLogoutClick = async () => {
     alert('로그아웃 하시겠습니까?');
     await signOut(auth);
     navigate('/');
   };
+
+  const getProfile = useSelector((state) => state.userInfo);
 
   return (
     <>
@@ -40,7 +40,18 @@ const Header = () => {
         <St.MenuWrapper>
           {currentUser ? (
             <>
-              <St.HeaderMenu2 onClick={handleProfileClick}>프로필</St.HeaderMenu2>
+              <St.Img
+                id="PROFILE IMG"
+                src={
+                  getProfile.photoURL
+                    ? getProfile.photoURL
+                    : 'https://freevector-images.s3.amazonaws.com/uploads/vector/preview/41311/FreeVectorWorld_Tourism_Day_Backgroundyc0622_generated.jpg'
+                }
+                onClick={() => {
+                  navigate('/mypage');
+                }}
+              />
+              <UserInfoModal />
               <St.HeaderMenu2 onClick={handleLogoutClick}>로그아웃</St.HeaderMenu2>
             </>
           ) : (
@@ -99,5 +110,15 @@ const St = {
     cursor: pointer;
     padding: 1px 6px;
     font-size: 0.8333rem;
+    display: flex;
+    align-items: center;
+    margin-left: 5px;
+  `,
+  Img: styled.img`
+    background-color: white;
+    width: 50px;
+    height: 50px;
+    border-radius: 100%;
+    cursor: pointer;
   `
 };
