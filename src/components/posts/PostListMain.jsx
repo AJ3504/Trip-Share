8e6db9b03afca2e17f3eaea6dffb4f7e52efd472
AJ3504 <plzgimmeajob@gmail.com>
2023-07-21 16:28:02 +0900

    Fix/synchronizing with dev

--- src/components/posts/PostListMain.jsx
+++ src/components/posts/PostListMain.jsx
@@ -1,14 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { collection, getDocs, query } from 'firebase/firestore';
-import { db } from '../../service/firebase';
+import React, { useEffect } from 'react';
 import { styled } from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { __getPostsSlice, __postsSlice, readPost } from '../../redux/modules/postsSlice';
+import { __getPostsSlice, __postsSlice } from '../../redux/modules/postsSlice';
 
-const PostListMain = ({ openSide, option }) => {
-  //useSelector
-
+const PostListMain = ({ openSide, option, position }) => {
+  //hooks
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -29,7 +26,6 @@ const PostListMain = ({ openSide, option }) => {
     return <h1>오류가 발생했어요</h1>;
   }
 
-  //Event Handler
   const onPostClick = (post) => {
     navigate(`/detail/${post.id}`, {
       state: {
@@ -39,20 +35,28 @@ const PostListMain = ({ openSide, option }) => {
     });
   };
 
+  const statedPosts = postsData.filter(
+    (post) =>
+      post.markerPosition.lat > position.swLat &&
+      post.markerPosition.lat < position.neLat &&
+      post.markerPosition.lng > position.swLng &&
+      post.markerPosition.lng < position.neLng
+  );
+
   return (
     <StSideBox>
       <button onClick={openSide}>닫기</button>
-      {postsData
+      {statedPosts
         .filter((post) => post.category === `${option}`)
         .map((post) => {
           return (
             <div key={post.id} style={{ border: 'solid', margin: '10px', padding: '10px' }}>
               <ul>
                 <li>{post.category}</li>
-                <br />
                 <li>{post.postTitle}</li>
                 <li>{post.postBody}</li>
-                {/* <img src={post.postImg} /> */}
+                <li>위도{post.markerPosition.lat}</li>
+                <li>경도{post.markerPosition.lng}</li>
                 <button onClick={() => onPostClick(post)}>상세보기</button>
               </ul>
             </div>
@@ -66,7 +70,7 @@ export default PostListMain;
 
 export const StSideBox = styled.ul`
   background-color: lightblue;
-  width: 400px;
+  width: 20%;
   height: 100%;
   right: 0px;
   position: absolute;
