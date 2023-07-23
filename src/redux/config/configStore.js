e6905ccfb246ff9cf586a97a883a15787850945f
AJ3504 <plzgimmeajob@gmail.com>
2023-07-23 15:09:04 +0900

    Other/deleted unused files, imports

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import postsSlice from '../modules/postsSlice';
 import userInfoSlice, { getUserProfile } from '../modules/userInfoSlice';
-import writerInfosSlice from '../modules/writerInfosSlice';
 
 const store = configureStore({
   reducer: {
     postsSlice,
-    writerInfosSlice: writerInfosSlice,
     userInfo: userInfoSlice
   }
 });
